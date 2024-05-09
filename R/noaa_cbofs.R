baad6edae4d24d153de1b5b776df00983f0fe9e3
Weston Slaughter <wslaught@umd.edu>
2024-05-09 16:17:15 -0400

    noaa cbofs looping in time

--- R/noaa_cbofs.R
+++ R/noaa_cbofs.R
@@ -4,12 +4,14 @@ library(ncdf4)
 library(CFtime)
 library(lattice)
 library(RColorBrewer)
+library(dplyr)
+library(raster)
 
 # set path and filename
 ncpath <- "./data/nc_files/"
 ncname <- "nos.cbofs.vibrioprob.20200818.t00z"  
 ncfname <- paste(ncpath, ncname, ".nc", sep="")
-dname <- "temp1mday1"  # note: tmp means temperature (not temporary)
+dname <- "sal1mday1"  # note: tmp means temperature (not temporary)
 
 # open a netCDF file
 ncin <- nc_open(ncfname)
@@ -24,7 +26,7 @@ lat <- ncvar_get(ncin,"lat_rho")
 nlat <- dim(lat)
 head(lat)
 
-lat_lon = 
+# lat_lon = 
 
 # convert ROMS XI, ETA curvilinear coords into lat, long 
 # following: https://www.myroms.org/forum/viewtopic.php?t=295
@@ -83,100 +85,82 @@ tmp_array[tmp_array==fillvalue$value] <- NA
 
 length(na.omit(as.vector(tmp_array[,,1])))
 
-# get a single slice or layer (January)
-m <- 1
-tmp_slice <- tmp_array[,,m]
-dim(tmp_slice)
 
-# vectorize lat, long?
-lat_vec = c()
-for(r in 1:nrow(lat)) {
-  for(c in 1:ncol(lat)) {
-    lat_vec = c(lat_vec, lat[r,c])
+times_vec = list(rep(c(""), 24))
+
+# loop through time
+for(t in 1:2) {
+  
+  # get a single slice or layer (January)
+  # m <- 1
+  var <- tmp_array[,,t]
+  dim(var)
+  
+  # vectorize lat, long?
+  lat_vec = c()
+  for(r in 1:nrow(lat)) {
+    for(c in 1:ncol(lat)) {
+      lat_vec = c(lat_vec, lat[r,c])
+    }
   }
-}
-
-# vectorize lon, long?
-lon_vec = c()
-for(r in 1:nrow(lon)) {
-  for(c in 1:ncol(lon)) {
-    lon_vec = c(lon_vec, lon[r,c])
+  
+  # vectorize lon, long?
+  lon_vec = c()
+  for(r in 1:nrow(lon)) {
+    for(c in 1:ncol(lon)) {
+      lon_vec = c(lon_vec, lon[r,c])
+    }
   }
-}
-
-# vectorize var, varg?
-var = tmp_slice
-var_vec = c()
-for(r in 1:nrow(var)) {
-  for(c in 1:ncol(var)) {
-    var_vec = c(var_vec, var[r,c])
+  
+  # vectorize var, varg?
+  var_vec = c()
+  for(r in 1:nrow(var)) {
+    for(c in 1:ncol(var)) {
+      var_vec = c(var_vec, var[r,c])
+    }
   }
+  
+  # replace netCDF fill values with NA's
+  lon_vec[lon_vec==fillvalue$value] <- NA
+  length(na.omit(as.vector(lon_vec)))
+  lon_vec_clean = na.omit(as.vector(lon_vec))
+  
+  # replace netCDF fill values with NA's
+  lat_vec[lat_vec==fillvalue$value] <- NA
+  length(na.omit(as.vector(lat_vec)))
+  lat_vec_clean = na.omit(as.vector(lat_vec))
+  
+  # replace netCDF fill values with NA's
+  var_vec[var_vec==fillvalue$value] <- NA
+  length(na.omit(as.vector(var_vec)))
+  var_vec_clean = na.omit(as.vector(var_vec))
+  
+  # create dataframe -- reshape data
+  # matrix (nlon*nlat rows by 2 cols) of lons and lats
+  lonlat <- cbind(lon_vec, lat_vec, var_vec)
+  dim(lonlat)
+  
+  lonlat <- as.data.frame(lonlat)
+  colnames(lonlat) <- c("lon", "lat", "var")
+  
+  lonlat_order <- lonlat %>%
+    arrange(lon, lat, var) %>%
+    filter(!is.na(lon))
+  
+  print(paste("saving into time vec for time:", t))
+  times_vec[[t]] = lonlat_order
+  
+  colnames(lonlat_order) <- c("x", "y", "z")
+  var_sf <- sf::st_as_sf(x = as.data.frame(lonlat_order), 
+                     coords = c("x", "y"),
+                     crs = 4326)
+  
+  # plot(etd_sf, main = NULL)
+  # title("\n\nChesapeake Bay Salinity, August 18 2020\nNOAA CBOFS from THREDDS Archive netCDF\n\n\n\n\n\n\n\n")
+  
+  ext <- floor(extent(var_sf))
+  rr <- raster(ext, res=0.019)
+  rr <- rasterize(var_sf, rr, field="z")
+  
+  plot(rr)
 }
-
-# replace netCDF fill values with NA's
-lon_vec[lon_vec==fillvalue$value] <- NA
-length(na.omit(as.vector(lon_vec)))
-lon_vec_clean = na.omit(as.vector(lon_vec))
-
-# replace netCDF fill values with NA's
-lat_vec[lat_vec==fillvalue$value] <- NA
-length(na.omit(as.vector(lat_vec)))
-lat_vec_clean = na.omit(as.vector(lat_vec))
-
-# replace netCDF fill values with NA's
-var_vec[var_vec==fillvalue$value] <- NA
-length(na.omit(as.vector(var_vec)))
-var_vec_clean = na.omit(as.vector(var_vec))
-
-
-# quick map
-image(lon_vec_clean, lat_vec_clean, var_vec) #, col=rev(brewer.pal(10,"RdBu")))
-
-
-# create dataframe -- reshape data
-# matrix (nlon*nlat rows by 2 cols) of lons and lats
-lonlat <- cbind(lon_vec, lat_vec, var_vec)
-dim(lonlat)
-
-# lonlat <- as.data.frame(lonlat)
-colnames(lonlat) <- c("lon", "lat", "var")
-
-library(dplyr)
-lonlat_order <- lonlat %>%
-  arrange(lon, lat, var) %>%
-  filter(!is.na(lon))
-
-# quick map
-image(lonlat_order$lon, lonlat_order$lat, lonlat_order$var) #, col=rev(brewer.pal(10,"RdBu")))
-
-tmp_cheat = tmp_array[,,1]
-
-rownames(tmp_cheat) <- lon_vec
-colnames(tmp_cheat) <- lat_vec
-
-levelplot(tmp_array[,,1], ~ lon_vec * lat_vec)
-
-colnames(lonlat_order) <- c("x", "y", "z")
-
-library(raster)
-
-etd_sf <- sf::st_as_sf(x = as.data.frame(lonlat_order), 
-                   coords = c("x", "y"),
-                   crs = 4326)
-
-plot(etd_sf, main = NULL)
-title("\n\nChesapeake Bay Salinity, August 18 2020\nNOAA CBOFS from THREDDS Archive netCDF\n\n\n\n\n\n\n\n")
-
-ext <- floor(extent(etd_sf))
-rr <- raster(ext, res=0.021)
-rr <- rasterize(etd_sf, rr, field="z")
-
-plot(rr)
-
-dfr <- rasterFromXYZ(etd_sf, res=c(NA, 1))
-
-levelplot(z~x*y, data = lonlat_order, region = TRUE)
-
-
-
-          
